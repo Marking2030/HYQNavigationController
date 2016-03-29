5e1cba742a1bbfd32895925a3123fc606732dc18
Mars2030 <528170939@qq.com>
2016-03-29 17:08:08 +0800

    Initial Commit

---
+++ HYQNavigationController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYQNavigationController
+//
+//  Created by 黄永强 on 16/3/29.
+//  Copyright © 2016年 黄永强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYQNavigationController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HYQNavigationController
+//
+//  Created by 黄永强 on 16/3/29.
+//  Copyright © 2016年 黄永强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
